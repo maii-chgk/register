c17c0f8875af4bc0b2ae086839546ca343185c3b
Jury Razumau <jury.razumau@zendesk.com>
2021-06-21 23:30:06 +0200

    add and remove from discourse group

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,9 @@
 class Person < ApplicationRecord
   has_and_belongs_to_many :assemblies, -> { distinct }
 
+  after_update set_discourse_role
+  after_destroy unset_discourse_role
+
   has_paper_trail
 
   rails_admin do
@@ -20,5 +23,15 @@ class Person < ApplicationRecord
     configure :start_date do
       label "Дата вступления"
     end
+  end
+
+  private
+
+  def set_discourse_role
+    DiscourseClient.add_to_group(email, DiscourseClient::MAIN_GROUP)
+  end
+
+  def unset_discourse_role
+    DiscourseClient.remove_from_group(email, DiscourseClient::MAIN_GROUP)
   end
 end
--- lib/discourse/client.rb
+++ lib/discourse/client.rb
@@ -2,58 +2,100 @@ require 'date'
 require_relative '../connection_manager'
 
 class DiscourseClient
-  DEFAULT_GROUP = "organization_members"
+  MAIN_GROUP = "organization_members"
 
-  def self.list_group_members(group_name: DEFAULT_GROUP)
-    query = <<~SQL
-      select users.name, ue.email
-      from users
-      join group_users on users.id = group_users.user_id
-      join groups on group_users.group_id = groups.id
-      join user_emails ue on users.id = ue.user_id
-      where groups.name = $1
-    SQL
+  class << self
 
-    execute(query, group_name).to_a
-  end
+    def list_group_members(group_name: MAIN_GROUP)
+      query = <<~SQL
+        select users.name, ue.email
+        from users
+        join group_users on users.id = group_users.user_id
+        join groups on group_users.group_id = groups.id
+        join user_emails ue on users.id = ue.user_id
+        where groups.name = $1
+      SQL
 
-  def self.list_voters(day, group_name: DEFAULT_GROUP)
-    group_filter = if group_name.nil?
-                     ""
-                   else
-                     "where p.groups = '#{group_name}'"
-                   end
+      execute(query, group_name).to_a
+    end
 
-    query = <<~SQL
-      with poll_dates as (
-          select p.id, min(poll_votes.created_at) as poll_start,
-                 max(poll_votes.created_at) as poll_finish
-          from polls p
-          join poll_votes on p.id = poll_votes.poll_id
-          #{group_filter}
-          group by p.id
-      )
-      
-      select distinct ue.email
-      from poll_dates pd
-      join poll_votes on pd.id = poll_votes.poll_id
-      join user_emails ue on poll_votes.user_id = ue.user_id
-      where poll_start >= $1 and poll_finish < $2
-    SQL
+    def list_voters(day, group_name: MAIN_GROUP)
+      group_filter = if group_name.nil?
+                       ""
+                     else
+                       "where p.groups = '#{group_name}'"
+                     end
 
-    next_day = Date.parse(day) + 1
-    execute(query, [day, next_day]).to_a
-  end
+      query = <<~SQL
+        with poll_dates as (
+            select p.id, min(poll_votes.created_at) as poll_start,
+                   max(poll_votes.created_at) as poll_finish
+            from polls p
+            join poll_votes on p.id = poll_votes.poll_id
+            #{group_filter}
+            group by p.id
+        )
+        
+        select distinct ue.email
+        from poll_dates pd
+        join poll_votes on pd.id = poll_votes.poll_id
+        join user_emails ue on poll_votes.user_id = ue.user_id
+        where poll_start >= $1 and poll_finish < $2
+      SQL
 
-  def self.add_to_group(email, group)
+      next_day = Date.parse(day) + 1
+      execute(query, [day, next_day]).to_a
+    end
 
-  end
+    def add_to_group(email, group_name)
+      group_id = fetch_group_id(group_name)
+      user_id = fetch_user_id(email)
+      query = <<~SQL
+        insert into groups_users (group_id, user_id, created_at, updated_at)
+        values ($1, $2, now(), now())
+      SQL
 
-  def self.execute(query, params)
-    params = [params] unless params.kind_of?(Array)
+      execute(query, [group_id, user_id])
+    end
 
-    ConnectionManager.connection_pool.with do |connection|
-      connection.exec_params(query, params)
+    def remove_from_group(email, group_name)
+      group_id = fetch_group_id(group_name)
+      user_id = fetch_user_id(email)
+      query = <<~SQL
+        delete from group_users
+        where user_id = $1
+            and group_id = $2;
+      SQL
+
+      execute(query, [group_id, user_id])
+    end
+
+    def fetch_group_id(group_name)
+      query = <<~SQL
+        select id
+        from groups
+        where groups.name = $1;
+      SQL
+
+      execute(query, group_name)
+    end
+
+    def fetch_user_id(email)
+      query = <<~SQL
+        select user_id
+        from user_emails
+        where email = $1
+      SQL
+
+      execute(query, email)
+    end
+
+    def execute(query, params)
+      params = [params] unless params.kind_of?(Array)
+
+      ConnectionManager.connection_pool.with do |connection|
+        connection.exec_params(query, params)
+      end
     end
   end
 end
