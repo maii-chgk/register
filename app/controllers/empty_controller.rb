795cc4643d9677ee05de253423b24c2827691d32
Jury Razumau <jury.razumau@zendesk.com>
2022-04-16 19:23:47 +0200

    remove removed members from count

--- app/controllers/empty_controller.rb
+++ app/controllers/empty_controller.rb
@@ -1,6 +1,6 @@
 class EmptyController < ApplicationController
   def index
-    count = Person.where(accepted: true).count
+    count = Person.where(accepted: true, end_date: nil).count
     render inline: "В МАИИ #{count} членов."
   end
 end
