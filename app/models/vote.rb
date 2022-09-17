6710724c7bcc6676395a960860d42840074db215
Jury Razumau <jury.razumau@zendesk.com>
2022-09-17 23:39:20 +0200

    add task to import all votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,6 +9,14 @@ class Vote < ApplicationRecord
 
   has_paper_trail
 
+  ELECTRONIC_VOTING_DATES = ["2021-07-23",
+                             "2021-08-01", "2021-08-06", "2021-08-15", "2021-08-29",
+                             "2021-09-12", "2021-09-14",
+                             "2022-01-05",
+                             "2022-08-04", "2022-08-21", "2022-08-22", "2022-08-23",
+                             "2022-09-04"]
+  ASSEMBLIES_DATES = ["2021-05-29", "2021-07-03", "2022-04-30", "2022-08-28"]
+
   def self.assemblies_between(first_date, second_date)
     Vote.where(["date between ? and ?", first_date, second_date])
       .assembly
