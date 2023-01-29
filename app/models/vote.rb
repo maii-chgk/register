6cbe78fb803929c3d101710af7bbb8e2c54f7966
Jury Razumau <jury.razumau@zendesk.com>
2023-01-29 19:09:17 +0100

    remove dates

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,14 +5,6 @@ class Vote < ApplicationRecord
 
   has_paper_trail
 
-  ELECTRONIC_VOTING_DATES = ["2021-07-23",
-                             "2021-08-01", "2021-08-06", "2021-08-15", "2021-08-29",
-                             "2021-09-12", "2021-09-14",
-                             "2022-01-05",
-                             "2022-08-04", "2022-08-21", "2022-08-22", "2022-08-23",
-                             "2022-09-04"]
-  ASSEMBLIES_DATES = ["2021-05-29", "2021-07-03", "2022-04-30", "2022-08-28", "2022-12-18"]
-
   rails_admin do
     exclude_fields :created_at, :updated_at, :id
 
