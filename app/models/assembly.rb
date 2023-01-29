e0a73a1925185bb723d6de7fd1cc7ca0dfa593ea
Jury Razumau <jury.razumau@zendesk.com>
2023-01-29 19:06:05 +0100

    add labels for assembly and voting session

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -4,4 +4,19 @@ class Assembly < ApplicationRecord
   def self.for_date(date)
     Assembly.where("start_date <= ? and end_date >= ?", date, date).first
   end
+
+  rails_admin do
+    exclude_fields :created_at, :updated_at, :id
+
+    label "Общее собрание"
+    label_plural "Общие собрания"
+
+    configure :start_date do
+      label "Дата начала"
+    end
+
+    configure :end_date do
+      label "Дата окончания"
+    end
+  end
 end
--- app/models/voting_session.rb
+++ app/models/voting_session.rb
@@ -2,4 +2,19 @@ class VotingSession < ApplicationRecord
   def self.for_date(date)
     VotingSession.where("start_date <= ? and end_date >= ?", date, date).first
   end
+
+  rails_admin do
+    exclude_fields :created_at, :updated_at, :id
+
+    label "Электронное голосование"
+    label_plural "Электронные голосования"
+
+    configure :start_date do
+      label "Дата начала"
+    end
+
+    configure :end_date do
+      label "Дата окончания"
+    end
+  end
 end
