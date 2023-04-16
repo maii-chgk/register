e3cede48d327fa5cf38d9857ce18cca9fb45dfbe
Jury Razumau <mail@razumau.net>
2023-04-16 14:00:51 +0200

    use ceiling to round up quorums

--- app/controllers/empty_controller.rb
+++ app/controllers/empty_controller.rb
@@ -2,6 +2,8 @@ class EmptyController < ApplicationController
   def index
     count = Person.members_count
     quorum_count = Person.count_toward_quorum
-    render inline: "В МАИИ #{count} членов. Для кворума ОС учитываются #{quorum_count}. Кворум — #{quorum_count / 4}. Кворум для изменения устава — #{quorum_count / 2}."
+    quorum = (Float(quorum_count) / 4).ceil
+    statute_quorum = (Float(quorum_count) / 2).ceil
+    render inline: "В МАИИ #{count} членов. Для кворума ОС учитываются #{quorum_count}. Кворум — #{quorum}. Кворум для изменения устава — #{statute_quorum}."
   end
 end
